e608e3b10d901983c202fe251968bb23618416d3
Alberti Luigy <albertiluigy@gmai.com>
2026-08-04 07:30:08 -0300

    Classe Departament

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,16 +3,13 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Departament;
 
 public class Program {
 
     public static void main(String[] args) {
-        Connection conn = null;
-        try {
-            conn = DB.getConnection();
-            System.out.println("Infraestrutura JDBC pronta. Conexao com o banco estabelecida.");
-        } finally {
-            DB.closeConnection();
-        }
+        
+        Departament obj = new Departament(1, "Oloco");
+        System.out.println(obj);
     }
 }
---
+++ src/main/java/model/entities/Departament.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departament implements Serializable {
+
+    private static final long serialVersionUID = 1L; // Versão serial do objeto
+    
+    private Integer id;
+    private String name;
+
+    public Departament() {
+    }
+
+    public Departament(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() { // Sobrescrita do método hashCode
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) { // Sobrescrita do método equals
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Departament other = (Departament) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Departament [id=" + id + ", name=" + name + "]";
+    }
+}
