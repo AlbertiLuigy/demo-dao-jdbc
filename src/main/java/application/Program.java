6a4e3c75aec994e3339a6548bbd90ba4c069ddf6
Alberti Luigy <albertiluigy@gmai.com>
2026-08-04 07:50:45 -0300

    Interfaces Departament e Seller implements

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,15 +1,11 @@
 package application;
 
-import java.sql.Connection;
-
 import db.DB;
 import model.entities.Departament;
 
 public class Program {
 
     public static void main(String[] args) {
-        
-        Departament obj = new Departament(1, "Oloco");
-        System.out.println(obj);
+      
     }
 }
